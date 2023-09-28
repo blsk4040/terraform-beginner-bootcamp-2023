aca10e48011b1b7ec65bb372e9d166460eea1bac
blsk4040 <141501109+blsk4040@users.noreply.github.com>
2023-09-28 13:54:55 +0000

    #13 migrate local state to terraform cloud using terraform cloud block (#14)

--- main.tf
+++ main.tf
@@ -1,4 +1,12 @@
 terraform {
+  cloud {
+    organization = "blsk4040"
+
+    workspaces {
+      name = "terra-house-1"
+    }
+  }
+
   required_providers {
     random = {
       source = "hashicorp/random"
